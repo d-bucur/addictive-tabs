208acc0cbd9877665529bc7eea987e10e30867d2
Dorian Bucur <sirdorius@gmail.com>
2023-07-26 23:23:35 +0200

    changed icon

--- src/options/Options.vue
+++ src/options/Options.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import { storageDemo } from '~/logic/storage'
 </script>
 
 <template>
@@ -7,8 +6,6 @@ import { storageDemo } from '~/logic/storage'
     <img src="/assets/icon.svg" class="icon-btn mx-2 text-2xl" alt="extension icon">
     <div>Options</div>
     <SharedSubtitle />
-
-    <input v-model="storageDemo" class="border border-gray-400 rounded px-2 py-1 mt-2">
 
     <div class="mt-4">
       Powered by Vite <pixelarticons-zap class="align-middle inline-block" />
